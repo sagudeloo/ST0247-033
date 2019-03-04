bd299a0e8fb7d135d070cece40a9dabd4ea1ac4d
sagudeloo <sagudeloo@eafit.edu.co>
2019-03-03 21:13:56 -0500

    Solucion probada del punto 2

--- laboratorios/lab02/codigo/punto_2/Queens.java
+++ laboratorios/lab02/codigo/punto_2/Queens.java
@@ -1,52 +1,76 @@
-import java.lang.reflect.Array;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Queens {
 
-    int [] board;
-    int nQueens;
-    int [] deadPoint;
-    int nSolutions = 0;
-    LinkedList<Integer> possible;
+    private int [] board;
+    private int nQueens;
+    private int [][] deadPoint;
+    private int nSolutions = 0;
+    private LinkedList<Integer> possible;
 
-    public Queens(int nQueens){
+    private Queens(int nQueens, int [][] deadPoint){
         board = new int[nQueens];
+        this.deadPoint = deadPoint;
         this.nQueens = nQueens;
         possible = new LinkedList<>();
-        for (int i = 0; i < nQueens; i++) {
+        for (int i = 0, j = nQueens-1; j >= 0 && i < nQueens; i++, j--) {
             possible.add(i);
-            deadPoint[i] = -1;
         }
     }
 
-    public void generateBoard(int k){
+    private void generateBoard(int k){
         if(k == nQueens){
-            nSolutions++;
+            if (checkBoard(k-1)) {
+                nSolutions++;
+            }
+        }else {
+            for (int i = 0; i < possible.size(); i++) {
+                board[k] = possible.pop();
+                if (checkBoard(k)) {
+                    generateBoard(k + 1);
+                }
+                possible.add(board[k]);
+            }
         }
-        for (int i = 0; i < nQueens; i++){
-            board[k] = possible.pop();
-            if(checkBoard(k)) generateBoard(k+1);
-            possible.add(board[k]);
-            board[k] = possible.removeFirst();
-        }
-
     }
 
-    public boolean checkBoard(int k){
-        if ((board[k] == deadPoint[k]) || (deadPoint[k] == -1))
+    private boolean checkBoard(int k){
+        if (deadPoint[k][board[k]] == 1) return false;
         for (int i = 0; i < k; i++) {
-            if (Math.abs(board[i]-board[k]) == Math.abs(i-k)) return false;
+            if (board[i] == board[k] || Math.abs(board[i]-board[k]) == Math.abs(i-k)) return false;
         }
         return true;
+    }
+
+    private void printBoard() {
+        int n = board.length;
+        System.out.print("    ");
+        for (int i = 0; i < n; ++i)
+            System.out.print(i + " ");
+        System.out.println("\n");
+        for (int i = 0; i < n; ++i) {
+            System.out.print(i + "   ");
+            for (int j = 0; j < n; ++j)
+                System.out.print((board[i] == j ? "Q" : "#") + " ");
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int nQueens = sc.nextInt();
+        sc.nextLine();
         String row;
-        String [] column;
-        Queens queens = new Queens(nQueens);
+        int [][] deadPoint = new int[nQueens][nQueens];
+        for (int i = 0; i < nQueens; i++) {
+            row = sc.nextLine();
+            for (int j = 0; j < nQueens; j++) {
+                if (row.charAt(j) == '*')deadPoint[i][j] = 1;
+            }
+        }
+        Queens queens = new Queens(nQueens, deadPoint);
         queens.generateBoard(0);
         System.out.println(queens.nSolutions);
 
